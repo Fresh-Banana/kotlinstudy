99d179676d47f7ea9dfd2cb5890ab41d557991ba
junsu <s26022@gsm.hs.kr>
2026-08-02 12:01:14 +0900

    기초세팅

---
+++ src/main/kotlin/org/example/kotlinchating/KotlinchatingApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlinchating
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinchatingApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinchatingApplication>(*args)
+}
